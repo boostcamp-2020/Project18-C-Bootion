86921c8b273cda9dee81a861b090d1faae176622
Namjin Kim <domino8788@naver.com>
2020-11-22 16:31:01 +0900

    [#10] feat: User Model 객체를 export에 포함

--- backend/src/schemas/index.ts
+++ backend/src/schemas/index.ts
@@ -1,19 +1,20 @@
 import * as mongoose from 'mongoose';
 import { config } from 'dotenv';
+
 config();
 
-export const connect = () => {
-  if (process.env.NODE_ENV! == 'production') {
+export const connect = (): void => {
+  if (process.env.NODE_ENV !== 'production') {
     mongoose.set('debug', true);
   }
+
   let { MONGO_USERNAME, MONGO_PASSWORD } = process.env;
   MONGO_USERNAME = encodeURIComponent(MONGO_USERNAME);
   MONGO_PASSWORD = encodeURIComponent(MONGO_PASSWORD);
+
   mongoose.connect(
     `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@localhost:27017/admin`,
-    {
-      useNewUrlParser:true
-    },
+    { useNewUrlParser: true },
     (error) => {
       if (error) {
         console.log('몽고디비 연결 에러', error);
@@ -23,3 +24,5 @@ export const connect = () => {
     },
   );
 };
+
+export { default as UserModel } from './user';
