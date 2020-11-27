617fc094c815f14be658cb93f2bf2ed5aa00feb7
Yi SoJeong <soj980312@gmail.com>
2020-11-27 10:07:17 +0900

    [#36] style: Prettier 적용

--- frontend/src/components/atoms/Heading/Heading.stories.tsx
+++ frontend/src/components/atoms/Heading/Heading.stories.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import {Heading1, Heading2, Heading3} from '.';
+import { Heading1, Heading2, Heading3 } from '.';
 
 const desc = {
   component: Heading1,
@@ -9,24 +9,24 @@ const desc = {
 
 export const HEADING1 = (): JSX.Element => {
   const headingProps = {
-    handleValue: () => {}, 
-    text: {current: 'Heading1'},
+    handleValue: () => {},
+    text: { current: 'Heading1' },
   };
   return <Heading1 {...headingProps} />;
 };
 
 export const HEADING2 = (): JSX.Element => {
   const headingProps = {
-    handleValue: () => {}, 
-    text: {current: 'Heading2'},
+    handleValue: () => {},
+    text: { current: 'Heading2' },
   };
   return <Heading2 {...headingProps} />;
 };
 
 export const HEADING3 = (): JSX.Element => {
   const headingProps = {
-    handleValue: () => {}, 
-    text: {current: 'Heading3'},
+    handleValue: () => {},
+    text: { current: 'Heading3' },
   };
   return <Heading3 {...headingProps} />;
 };
