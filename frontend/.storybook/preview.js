7c940f57db1d8f6c64d6be22b3136ce3e4d5929d
Namjin Kim <domino8788@naver.com>
2020-11-30 17:40:09 +0900

    [#39] feat: storybook preview에 Recoil 환경적용

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,4 +1,5 @@
 import { Global, css } from '@emotion/react';
+import { RecoilRoot } from 'recoil';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -6,26 +7,28 @@ export const parameters = {
 
 export const decorators = [
   (Story) => (
-    <div
-      style={{
-        margin: '3em',
-        border: '1px solid #000000',
-        position: 'relative',
-      }}
-    >
-      <Global
-        styles={css`
-          /* Simple Reset CSS */
-          *,
-          *:before,
-          *:after {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-          }
-        `}
-      />
-      <Story />
-    </div>
+    <RecoilRoot>
+      <div
+        style={{
+          margin: '3em',
+          border: '1px solid #000000',
+          position: 'relative',
+        }}
+      >
+        <Global
+          styles={css`
+            /* Simple Reset CSS */
+            *,
+            *:before,
+            *:after {
+              margin: 0;
+              padding: 0;
+              box-sizing: border-box;
+            }
+          `}
+        />
+        <Story />
+      </div>
+    </RecoilRoot>
   ),
 ];
