0eb3fe1a088d24573716de777879d2c8260b0d04
Yi SoJeong <soj980312@gmail.com>
2020-12-02 08:49:09 +0900

    [#58] feat: Text 컴포넌트 생성

---
+++ frontend/src/components/atoms/Text/Text.tsx
@@ -0,0 +1,18 @@
+/** @jsx jsx */
+/** @jsxRuntime classic */
+import { jsx } from '@emotion/react';
+
+function Text(): JSX.Element {
+  return (
+    <div
+      css={{}}
+      contentEditable
+      suppressContentEditableWarning
+      onInput={() => {}}
+    >
+      asfd
+    </div>
+  );
+}
+
+export default Text;
