bff1156ee7696a85cc535af109130d2f29671165
skid901 <tlrudejr1993@naver.com>
2020-12-03 06:15:30 +0900

    [#66] feat(controllers): Page 컨트롤러 구현

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,1 @@
+export * as pageController from './page';
---
+++ backend/src/controllers/page.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+
+import { pageService } from '@/services';
+import { StatusCode } from '@/middlewares';
+
+export const create = async (req: Request, res: Response): Promise<any> => {
+  const page = await pageService.create({ title: req.body.title });
+  res.status(StatusCode.CREATED).json(page);
+};
+
+export const getAll = async (req: Request, res: Response): Promise<any> => {
+  const pages = await pageService.getAll();
+  res.status(StatusCode.OK).json(pages);
+};
+
+export const getOne = async (req: Request, res: Response): Promise<any> => {
+  const page = await pageService.getOne({ id: req.params.id });
+  res.status(StatusCode.OK).json(page);
+};
+
+export const update = async (req: Request, res: Response): Promise<any> => {
+  const page = await pageService.update({
+    id: req.params.id,
+    title: req.body.title,
+    blocks: req.body.blocks,
+  });
+  res.status(StatusCode.OK).json(page);
+};
+
+export const remove = async (req: Request, res: Response): Promise<any> => {
+  await pageService.remove({ id: req.params.id });
+  res.status(StatusCode.NO_CONTENT).json();
+};
