7e94c27c7efa1f8e95853920adb5a3bdc80dadea
skid901 <tlrudejr1993@naver.com>
2020-12-03 06:20:37 +0900

    [#66] refactor: App.ts 및 www.ts 수정
    - 에러 핸들링 미들웨어를 고려하여 에러 처리 로직 수정
    - www.ts 내 에러 핸들링 로직을 App.ts로 이동

--- backend/src/www.ts
+++ backend/src/www.ts
@@ -1,14 +1,11 @@
 import 'source-map-support/register';
 import { config } from 'dotenv';
-import express from 'express';
 
 import App from './App';
 
 config();
 
 const port: number = Number(process.env.BACKEND_PORT) || 3000;
-const app: express.Application = new App().app;
+const app = App.bootstrap();
 
-app
-  .listen(port, () => console.log(`Express server listening at ${port}`))
-  .on('error', (err) => console.error(err));
+app.listen(port);
