ce6c977aaf81d0f0cd94415bf5b965029f369e80
skid901 <tlrudejr1993@naver.com>
2020-12-04 01:48:53 +0900

    [#66] refactor(services/page.ts): page 서비스 함수 내 반환 타입 재설정

--- backend/src/services/page.ts
+++ backend/src/services/page.ts
@@ -1,8 +1,10 @@
+import { Document } from 'mongoose';
+
 import { Page, Block } from '@/models';
 import { generateId } from '@/utils';
 import { ErrorMessage } from '@/middlewares';
 
-export const create = async (params: { title: string }): Promise<any> => {
+export const create = async (params: { title: string }): Promise<Document> => {
   const page = new Page({
     id: generateId(),
     title: params.title,
@@ -11,14 +13,12 @@ export const create = async (params: { title: string }): Promise<any> => {
   return page;
 };
 
-export const getAll = async (): Promise<any> => {
-  return await Page.find().populate({ path: 'blocks' }).exec();
+export const getAll = async (): Promise<Document[]> => {
+  return await Page.find().populate('blocks').exec();
 };
 
-export const getOne = async (params: { id: string }): Promise<any> => {
-  const page = await Page.findOne({ id: params.id })
-    .populate({ path: 'blocks' })
-    .exec();
+export const getOne = async (params: { id: string }): Promise<Document> => {
+  const page = await Page.findOne({ id: params.id }).populate('blocks').exec();
 
   if (!page) {
     throw new Error(ErrorMessage.NOT_FOUND);
@@ -30,7 +30,7 @@ export const update = async (params: {
   id: string;
   title: string | null;
   blocks: string[] | null;
-}): Promise<any> => {
+}): Promise<Document> => {
   const update = { title: params.title, blocks: params.blocks };
   update.title ?? delete update.title;
   update.blocks ?? delete update.blocks;
@@ -51,7 +51,7 @@ export const update = async (params: {
   return page;
 };
 
-export const remove = async (params: { id: string }): Promise<any> => {
+export const remove = async (params: { id: string }): Promise<void> => {
   const page: any = await Page.findOne({ id: params.id }).exec();
   page?.blocks.forEach((blockId: string) =>
     Block.findOneAndDelete({ id: blockId }),
