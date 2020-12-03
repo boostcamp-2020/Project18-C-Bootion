598e0ac3646e2b11878ccfa9c09e45b66bf6e00f
skid901 <tlrudejr1993@naver.com>
2020-12-04 01:37:39 +0900

    [#66] refactor(controllers/page.ts): page 컨트롤러 함수 내 반환 타입 재설정

--- backend/src/controllers/page.ts
+++ backend/src/controllers/page.ts
@@ -3,22 +3,22 @@ import { Request, Response } from 'express';
 import { pageService } from '@/services';
 import { StatusCode } from '@/middlewares';
 
-export const create = async (req: Request, res: Response): Promise<any> => {
+export const create = async (req: Request, res: Response): Promise<void> => {
   const page = await pageService.create({ title: req.body.title });
   res.status(StatusCode.CREATED).json(page);
 };
 
-export const getAll = async (req: Request, res: Response): Promise<any> => {
+export const getAll = async (req: Request, res: Response): Promise<void> => {
   const pages = await pageService.getAll();
   res.status(StatusCode.OK).json(pages);
 };
 
-export const getOne = async (req: Request, res: Response): Promise<any> => {
+export const getOne = async (req: Request, res: Response): Promise<void> => {
   const page = await pageService.getOne({ id: req.params.id });
   res.status(StatusCode.OK).json(page);
 };
 
-export const update = async (req: Request, res: Response): Promise<any> => {
+export const update = async (req: Request, res: Response): Promise<void> => {
   const page = await pageService.update({
     id: req.params.id,
     title: req.body.title,
@@ -27,7 +27,7 @@ export const update = async (req: Request, res: Response): Promise<any> => {
   res.status(StatusCode.OK).json(page);
 };
 
-export const remove = async (req: Request, res: Response): Promise<any> => {
+export const remove = async (req: Request, res: Response): Promise<void> => {
   await pageService.remove({ id: req.params.id });
   res.status(StatusCode.NO_CONTENT).json();
 };
