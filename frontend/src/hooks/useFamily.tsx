8ea55af0f371993eba48a464e57ae70c67bc8ad6
domino8788 <domino8788@naver.com>
2020-12-03 14:54:22 +0900

    [#62] feat: useFamily hook에 getPrevBlock 함수 구현

--- frontend/src/hooks/useFamily.tsx
+++ frontend/src/hooks/useFamily.tsx
@@ -4,6 +4,7 @@ import { Block, Page, BlockType, BlockFamily } from '@/schemes';
 
 interface FamilyFunc {
   getNextBlock: () => Block | null;
+  getPrevBlock: () => Block | null;
 }
 
 const findLastDescendant = (targetBlock: Block) => {
@@ -57,6 +58,56 @@ const useFamily = (blockId: string): [BlockFamily, FamilyFunc] => {
       return siblings[blockIndex + 1];
     }
   };
+  const getPrevBlock = () => {
+    if (blockIndex) {
+      /** blockIndex가 0이 아니면 이전 형제에서 prev block을 찾을 수 있다. */
+      const prevSibling = siblings[blockIndex - 1];
+      if (prevSibling.children.length) {
+        /* 이전 형제에게 자식이 있다면 마지막 자식이 prev block 이다.  */
+        return prevSibling.children[prevSibling.children.length - 1];
+      }
+      /* 이전 형제에게 자식이 없다면 형제가 prev block 이다.  */
+      return prevSibling;
+    }
+    /* block이 첫번째 자식일 때. 부모가 이전 Block 이다. */
+    if (parent) {
+      switch (parent.type) {
+        case BlockType.COLUMN: {
+          /** 부모의 Type이 Column 이면 부모의 이전 형제의 마지막 자식이 이전 Block 이다. */
+          if (parentIndex) {
+            /** 부모의 index가 0이 아니면 이전 부모의 마지막 */
+            const prevParentChildren = parents[parentIndex - 1].children;
+            return prevParentChildren[prevParentChildren.length - 1];
+          }
+          /** 부모의 index가 0이면 조부모에 이전 Block이 있다. */
+          if (grandParent) {
+            /** grandParent가 있을 경우 조부모의 마지막 자손이 이전 Block 이다. */
+            if (grandParent.type === BlockType.GRID) {
+              /** 조부모가 GRID 타입인 경우 조부모의 부모는 Page 이다. */
+              const grandParentIndex = page.blockList.findIndex(
+                (_block) => _block.id === grandParent.id,
+              );
+              if (grandParentIndex) {
+                /** grandParentIndex 가 0이 아니면 prevGrandParent의 마지막 후손이 prev 블록이다. */
+                const prevGrandParent = page.blockList[grandParentIndex - 1];
+                return findLastDescendant(prevGrandParent);
+              }
+              /** grandParentIndex가 0이면 page의 blockList 중 맨 앞이라는 의미이므로 prev 블록이 없다. */
+              return null;
+            }
+            return grandParent;
+          }
+          /** grandParent가 없을 경우 이전 블록이 없다는 뜻이다. */
+          return null;
+        }
+        default:
+          return parent;
+      }
+    } else {
+      /** 현재 블록이 첫 블록이다. */
+      return null;
+    }
+  };
   return [
     {
       block,
@@ -71,6 +122,7 @@ const useFamily = (blockId: string): [BlockFamily, FamilyFunc] => {
     },
     {
       getNextBlock,
+      getPrevBlock,
     },
   ];
 };
