53d85e9868091184c357dad800ac199611d883af
skid901 <tlrudejr1993@naver.com>
2020-12-04 06:13:40 +0900

    [#66] refactor(services/page): 모델 변경 사항 반영하여 로직 수정
    - id 속성에 대해 mongoDB에서 자동으로 제공해주는 기능을 사용

--- backend/src/services/page.ts
+++ backend/src/services/page.ts
@@ -1,14 +1,10 @@
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 import { Page, Block } from '@/models';
-import { generateId } from '@/utils';
 import { ErrorMessage } from '@/middlewares';
 
 export const create = async (params: { title: string }): Promise<Document> => {
-  const page = new Page({
-    id: generateId(),
-    title: params.title,
-  });
+  const page = new Page({ title: params.title });
   await page.save();
   return page;
 };
@@ -18,7 +14,7 @@ export const getAll = async (): Promise<Document[]> => {
 };
 
 export const getOne = async (params: { id: string }): Promise<Document> => {
-  const page = await Page.findOne({ id: params.id }).populate('blocks').exec();
+  const page = await Page.findById(params.id).populate('blocks').exec();
 
   if (!page) {
     throw new Error(ErrorMessage.NOT_FOUND);
@@ -34,16 +30,9 @@ export const update = async (params: {
   const update = { title: params.title, blocks: params.blocks };
   update.title ?? delete update.title;
   update.blocks ?? delete update.blocks;
-
-  const page = await Page.findOneAndUpdate(
-    {
-      id: params.id,
-    },
-    update,
-    {
-      new: true,
-    },
-  ).exec();
+  const page = await Page.findByIdAndUpdate(params.id, update, {
+    new: true,
+  }).exec();
 
   if (!page) {
     throw new Error(ErrorMessage.NOT_FOUND);
@@ -52,9 +41,7 @@ export const update = async (params: {
 };
 
 export const remove = async (params: { id: string }): Promise<void> => {
-  const page: any = await Page.findOne({ id: params.id }).exec();
-  page?.blocks.forEach((blockId: string) =>
-    Block.findOneAndDelete({ id: blockId }),
-  );
-  await Page.findOneAndDelete({ id: params.id }).exec();
+  const page: any = await Page.findById(params.id).exec();
+  page && (await Block.deleteMany({ pageId: { $eq: page.id } }));
+  await Page.findByIdAndDelete(page.id).exec();
 };
