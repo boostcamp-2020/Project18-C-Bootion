22e68506c03d5c9da34ec77e2ed86414e4490b7a
domino8788 <domino8788@naver.com>
2020-12-04 13:09:13 +0900

    [#62] feat: 상하좌우 이동 시 caret 위치 반영 로직 구현

--- frontend/src/hooks/useCommand.tsx
+++ frontend/src/hooks/useCommand.tsx
@@ -1,24 +1,58 @@
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { focusState } from '@/stores';
 import { useFamily } from '@/hooks';
 import { Block } from '@/schemes';
+import { blockRefState } from '@/stores/dist/page';
 
 const useCommand = () => {
   const [focusId, setFocusId] = useRecoilState(focusState);
   const [, familyFunc] = useFamily(focusId);
+  const blockRef: any = useRecoilValue(blockRefState);
 
   const Dispatcher = (key: String) => {
+    const sel = window.getSelection();
+    const range = document.createRange();
     switch (key) {
       case 'ArrowUp':
       case 'ArrowLeft': {
         const prevBlock: Block = familyFunc.getPrevBlock();
-        if (prevBlock) setFocusId(prevBlock.id);
+        if (prevBlock) {
+          const ref = blockRef[prevBlock.id].current;
+          const { focusOffset } = window.getSelection();
+          ref.focus();
+          const { focusNode } = window.getSelection();
+          const { length } = ref.innerText;
+          setFocusId(prevBlock.id);
+          if (key === 'ArrowLeft') {
+            range.setStart(focusNode ?? ref, length);
+          } else if (focusOffset > length) {
+            range.setStart(focusNode ?? ref, length);
+          } else range.setStart(focusNode ?? ref, focusOffset);
+          range.collapse(true);
+          sel.removeAllRanges();
+          sel.addRange(range);
+        }
         break;
       }
       case 'ArrowDown':
       case 'ArrowRight': {
         const nextBlock: Block = familyFunc.getNextBlock();
-        if (nextBlock) setFocusId(nextBlock.id);
+        if (nextBlock) {
+          const ref = blockRef[nextBlock.id].current;
+          const { focusOffset } = window.getSelection();
+          ref.focus();
+          const { focusNode } = window.getSelection();
+          const { length } = ref.innerText;
+          setFocusId(nextBlock.id);
+          if (key === 'ArrowRight') {
+            range.setStart(focusNode ?? ref, 0);
+          } else if (focusOffset > length) {
+            range.setStart(focusNode ?? ref, length);
+          } else range.setStart(focusNode ?? ref, focusOffset);
+          range.collapse(true);
+          sel.removeAllRanges();
+          sel.addRange(range);
+        }
         break;
       }
     }
