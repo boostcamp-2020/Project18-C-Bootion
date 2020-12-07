e7030e6c55e643fab9541668d21bf620985121db
skid901 <tlrudejr1993@naver.com>
2020-12-07 12:31:55 +0900

    [#66] feat(models): block 모델 관련 타입, 메소드 추가 및 모델 모듈 등록

--- backend/src/models/block.ts
+++ backend/src/models/block.ts
@@ -1,11 +1,41 @@
-import { model, Schema } from 'mongoose';
+import { Document, Schema, model } from 'mongoose';
+
+export enum BlockType {
+  TEXT = 'text',
+}
+
+export interface Block {
+  _id?: string;
+  id?: string;
+  type?: BlockType;
+  value?: string;
+  pageId?: string;
+  parentIdList?: string[];
+  children?: Block[];
+}
+
+export interface BlockDoc extends Document {
+  type: BlockType;
+  value: string;
+  pageId: string;
+  parentIdList: string[];
+  children: any[];
+  addChild: (
+    this: BlockDoc,
+    child: BlockDoc,
+    targetIndex?: number,
+  ) => Promise<void>;
+  removeFromParent: (this: BlockDoc) => Promise<void>;
+  requestSave: (this: BlockDoc) => Promise<void>;
+}
 
 const BlockSchema = new Schema();
 BlockSchema.add({
   type: {
     type: String,
     required: true,
-    default: 'text',
+    enum: Object.values(BlockType),
+    default: BlockType.TEXT,
   },
   value: {
     type: String,
@@ -14,13 +44,55 @@ BlockSchema.add({
   pageId: {
     type: Schema.Types.ObjectId,
     ref: 'Page',
-    default: null,
   },
-  parentId: {
-    type: Schema.Types.ObjectId,
-    ref: 'Block',
-  },
+  parentIdList: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Block',
+    },
+  ],
   children: [BlockSchema],
 });
 
-export const Block = model('Block', BlockSchema);
+BlockSchema.methods.addChild = async function (
+  this: BlockDoc,
+  child: BlockDoc,
+  targetIndex?: number,
+): Promise<void> {
+  if (
+    (targetIndex !== 0 && !targetIndex) ||
+    targetIndex < 0 ||
+    targetIndex > this.children.length
+  ) {
+    targetIndex = this.children.length;
+  }
+  child.pageId = this.pageId;
+  child.parentIdList = [...this.parentIdList, this.id];
+  child.children.forEach((grandChild: BlockDoc, index: number) =>
+    child.addChild(grandChild, index),
+  );
+  this.children.splice(targetIndex, 0, child);
+};
+
+BlockSchema.methods.removeFromParent = async function (
+  this: BlockDoc,
+): Promise<void> {
+  const parent: BlockDoc = (this as any).parent();
+  if (!parent) return;
+  parent.children = parent.children.filter(
+    (_child: BlockDoc) => this.id !== _child.id,
+  );
+  await parent.requestSave();
+};
+
+BlockSchema.methods.requestSave = async function (
+  this: BlockDoc,
+): Promise<void> {
+  if (!this.parentIdList.length) {
+    await this.save();
+  } else {
+    await (this as any).ownerDocument().save();
+  }
+};
+
+export const BlockModel = model<BlockDoc>('Block', BlockSchema);
--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -1,6 +1,6 @@
 import mongoose, { ConnectionOptions } from 'mongoose';
 
-export const connect = () => {
+export const connect = (): void => {
   const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_DATABASE } = process.env;
   let ip = 'mongo';
 
@@ -27,5 +27,7 @@ export const connect = () => {
     .catch((err) => console.log('MongoDB connection failed', err));
 };
 
-export { Page } from './page';
-export { Block } from './block';
+export { PageModel } from './page';
+export type { Page, PageDoc } from './page';
+export { BlockModel } from './block';
+export type { BlockType, Block, BlockDoc } from './block';
