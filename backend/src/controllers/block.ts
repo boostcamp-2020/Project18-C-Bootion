0e7ce978cc758c5c4101460b6432c97bf42c808b
skid901 <tlrudejr1993@naver.com>
2020-12-07 12:44:33 +0900

    [#66] refactor: block 컨트롤러 및 라우터 리팩토링
    - GET / DELETE 메소드 삭제
    - 파라미터 상태 별 서비스 함수 호출 분기 로직 추가
    - 트랜잭션 처리 추가

--- backend/src/controllers/block.ts
+++ backend/src/controllers/block.ts
@@ -1,42 +1,55 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 import { blockService } from '@/services';
-import { StatusCode } from '@/middlewares';
+import { ErrorMessage, StatusCode, transactionHandler } from '@/middlewares';
+import { BlockDoc, PageDoc } from '@/models';
 
-export const create = async (req: Request, res: Response): Promise<void> => {
-  const { block, parent, page } = !req.body.parentId
-    ? await blockService.createToPage({
-        pageId: req.body.pageId,
-        targetId: req.body.targetId,
-      })
-    : await blockService.createToBlock({
-        parentId: req.body.parentId,
-        targetId: req.body.targetId,
-      });
-  res.status(StatusCode.CREATED).json({ block, parent, page });
-};
+export const create = transactionHandler(
+  async (req: Request, res: Response): Promise<void> => {
+    const { block, parent, page } = !req.body.parent
+      ? await blockService.createToPage({
+          pageId: req.body.pageId,
+          block: req.body.block,
+          targetIndex: req.body.targetIndex,
+        })
+      : await blockService.createToBlock({
+          parent: req.body.parent,
+          block: req.body.block,
+          targetIndex: req.body.targetIndex,
+        });
+    res.status(StatusCode.CREATED).json({ block, parent, page });
+  },
+);
 
-export const getOne = async (req: Request, res: Response): Promise<void> => {
-  const block = await blockService.getOne({ id: req.params.id });
-  res.status(StatusCode.OK).json(block);
-};
+export const updateOrRemove = transactionHandler(
+  async (req: Request, res: Response): Promise<void> => {
+    const params = {
+      pageId: req.body.pageId, // 페이지로 옮길 때
+      parent: req.body.parent, // 특정 블록으로 옮길 때
+      block: req.body.block,
+      targetIndex: req.body.targetIndex,
+      remove: req.body.remove, // 삭제할 때
+    };
+    let block: BlockDoc = null,
+      parent: BlockDoc | null = null,
+      page: PageDoc | null = null;
 
-export const update = async (req: Request, res: Response): Promise<void> => {
-  const params = {
-    id: req.params.id,
-    type: req.body.type,
-    value: req.body.value,
+    if (params.remove) {
+      await blockService.remove(params.block);
+      res.status(StatusCode.NO_CONTENT).json();
+      return;
+    }
 
-    pageId: req.body.pageId,
-    parentId: req.body.parentId,
-    targetId: req.body.targetId,
-  };
-
-  const block = await blockService.update(params);
-  res.status(StatusCode.OK).json(block);
-};
-
-export const remove = async (req: Request, res: Response): Promise<void> => {
-  await blockService.remove({ id: req.params.id });
-  res.status(StatusCode.NO_CONTENT).json();
-};
+    if (!params.pageId && !params.parent) {
+      block = await blockService.updateBlock(params.block);
+    } else if (params.pageId && !params.parent) {
+      delete params.parent;
+      [block, page] = await blockService.moveToPage(params);
+    } else if (!params.pageId && params.parent) {
+      [block, parent] = await blockService.moveToBlock(params);
+    } else {
+      throw new Error(ErrorMessage.BAD_REQUEST);
+    }
+    res.status(StatusCode.OK).json({ block, parent, page });
+  },
+);
--- backend/src/routes/block.ts
+++ backend/src/routes/block.ts
@@ -6,6 +6,4 @@ import { errorHandler } from '@/middlewares';
 export const blockRouter = Router();
 
 blockRouter.post('', errorHandler(blockController.create));
-blockRouter.get('/id/:id', errorHandler(blockController.getOne));
-blockRouter.patch('/id/:id', errorHandler(blockController.update));
-blockRouter.delete('/id/:id', errorHandler(blockController.remove));
+blockRouter.patch('', errorHandler(blockController.updateOrRemove));
