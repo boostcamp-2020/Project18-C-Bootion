c95b14ccfe3fdfc15efc95ba6455833dc849ddb5
Yi SoJeong <soj980312@gmail.com>
2020-12-07 11:42:07 +0900

    [#58] feat: value에따라 type 변환
    - 순서 리스트를 위해 listComponent추가
    - Type 변환하는 로직 리팩토링

--- frontend/src/components/atoms/BlockContent/BlockContent.tsx
+++ frontend/src/components/atoms/BlockContent/BlockContent.tsx
@@ -1,26 +1,24 @@
 /** @jsx jsx */
 /** @jsxRuntime classic */
 import { jsx, css, SerializedStyles } from '@emotion/react';
-import { useRef, useEffect, useState, FormEvent, KeyboardEvent } from 'react';
+import { useEffect, FormEvent, KeyboardEvent } from 'react';
 import { useRecoilState } from 'recoil';
 
 import { blockState } from '@/stores';
 import { Block, BlockType } from '@/schemes';
-import { regex } from '@utils/regex';
+import { regex, fontSize, placeHolder } from '@/utils/blockContent';
+import { ReactComponent as Toggle } from '@assets/toggle-default.svg';
 
 const isGridOrColumn = (block: Block): boolean =>
   block.type === BlockType.GRID || block.type === BlockType.COLUMN;
 
-const FontSize: { [key: string]: any } = {
-  heading1: 'xx-large',
-  heading2: 'x-large',
-  heading3: 'large',
-};
-
-const contentsCss = (block: Block): SerializedStyles => css`
+const blockContentCSS = css`
+  display: flex;
+  align-items: center;
+`;
+const editableDivCSS = (block: Block): SerializedStyles => css`
   margin: 5;
-  font-size: ${FontSize[block.type]};
-
+  font-size: ${fontSize[block.type]};
   display: ${!isGridOrColumn(block) ? 'flex' : 'none'};
   align-items: flex-start;
   max-width: 100%;
@@ -46,31 +44,32 @@ const contentsCss = (block: Block): SerializedStyles => css`
   }
 `;
 
+const listComponent: { [key: string]: any } = {
+  bulletedlist: <span>•</span>,
+  numberedlist: <span> 1. </span>,
+  togglelist: (
+    <span>
+      <Toggle />
+    </span>
+  ),
+  quote: <span>▕</span>,
+};
+
 function BlockContent(blockDTO: Block) {
   const [block, setBlock] = useRecoilState(blockState(blockDTO.id));
-  const renderBlock = block ?? blockDTO;
+  const renderBlock: Block = block ?? blockDTO;
 
-  // useValidation처럼 customHook으로
   useEffect(() => {
-    if (regex.h1.test(renderBlock.value)) {
+    const newType = Object.entries(regex).find((testRegex) =>
+      testRegex[1].test(renderBlock.value),
+    );
+    if (newType) {
       setBlock({
-        ...block,
-        type: BlockType.HEADING1,
+        ...renderBlock,
+        type: newType[0],
         value: '',
       });
     }
-    if (regex.h2.test(renderBlock.value))
-      setBlock({
-        ...block,
-        type: BlockType.HEADING2,
-        value: '',
-      });
-    if (regex.h3.test(renderBlock.value))
-      setBlock({
-        ...block,
-        type: BlockType.HEADING3,
-        value: '',
-      });
   }, [renderBlock.value]);
 
   const handleValue = (event: FormEvent<HTMLDivElement>) => {
@@ -80,21 +79,23 @@ function BlockContent(blockDTO: Block) {
     });
   };
 
-  const handleBackSpace = (event: KeyboardEvent<HTMLDivElement>) => {};
+  const handleBackSpace = (event: KeyboardEvent<HTMLDivElement>) => {
+    // console.log('>>>>', event);
+  };
 
   return (
-    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
-    <div
-      css={contentsCss(renderBlock)}
-      contentEditable
-      suppressContentEditableWarning
-      placeholder={
-        renderBlock.type === BlockType.TEXT ? "Type '/' for commands" : ''
-      }
-      onInput={handleValue}
-      onKeyPress={handleBackSpace}
-    >
-      {renderBlock.value}
+    <div css={blockContentCSS}>
+      {listComponent[renderBlock.type]}
+      <div
+        css={editableDivCSS(renderBlock)}
+        contentEditable
+        suppressContentEditableWarning
+        placeholder={placeHolder[renderBlock.type]}
+        onInput={handleValue}
+        onKeyUp={handleBackSpace}
+      >
+        {renderBlock.value}
+      </div>
     </div>
   );
 }
