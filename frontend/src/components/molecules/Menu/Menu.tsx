f592d2ee99c1c1248fd57121c506af4054d3f590
skid901 <tlrudejr1993@naver.com>
2020-12-10 05:32:44 +0900

    [#88] refactor: 메뉴 css 수정

--- frontend/src/components/molecules/Menu/Menu.tsx
+++ frontend/src/components/molecules/Menu/Menu.tsx
@@ -1,37 +1,37 @@
 /** @jsx jsx */
 /** @jsxRuntime classic */
-import { jsx, css, SerializedStyles } from '@emotion/react';
+import { jsx, css } from '@emotion/react';
 import { Suspense } from 'react';
 import { useRecoilState } from 'recoil';
 
-import { pagesState } from '@/stores';
+import { pagesState, staticMenuToggleState } from '@/stores';
 import { createPage } from '@/utils';
 
-const wrapperCss = (): SerializedStyles => css`
-  width: 30px;
+const wrapperCss = (staticToggle: boolean) => css`
   width: 240px;
   height: 80vh;
-  border: 1px solid #999999;
   border-radius: 3px;
-  background-color: #ffffff;
-  box-shadow: 0 0 30px 1px #aaaaaa;
+  background: rgb(247, 246, 243);
+  ${!staticToggle ? 'box-shadow: 0 0 10px 1px #aaaaaa' : ''};
 `;
 
 interface Props {}
 
 function Menu({}: Props): JSX.Element {
   const [pages, setPages] = useRecoilState(pagesState);
+  const [staticToggle, setStaticToggle] = useRecoilState(staticMenuToggleState);
 
   const handleClick = () => {
-    (async () => {
+    const createAndFetchPage = async () => {
       const { pages: updatedPages } = await createPage();
       setPages(updatedPages);
-    })();
+    };
+    createAndFetchPage();
   };
 
   return (
-    <div css={wrapperCss()}>
-      <div>menu</div>
+    <div css={wrapperCss(staticToggle)}>
+      <div>WORKSPACE</div>
       <button type="button" onClick={handleClick}>
         +
       </button>
