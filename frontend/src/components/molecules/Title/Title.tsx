62045f73f8ae44bba594a1a563645b9615224efa
skid901 <tlrudejr1993@naver.com>
2020-12-10 08:36:06 +0900

    [#88] refactor(components/molecules/Title): 제목 변경 로직 추가 및 css 수정

--- frontend/src/components/molecules/Title/Title.tsx
+++ frontend/src/components/molecules/Title/Title.tsx
@@ -2,8 +2,9 @@
 /** @jsxRuntime classic */
 import { jsx, css } from '@emotion/react';
 
-import { useRecoilValue } from 'recoil';
-import { pageState } from '@/stores';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { pagesState, selectedPageState } from '@/stores';
+import { ChangeEvent } from 'react';
 
 const wrapperCss = () => css`
   padding-left: calc(96px + env(safe-area-inset-left));
@@ -20,19 +21,42 @@ const titleCss = () => css`
   font-weight: 700;
   line-height: 1.2;
   font-size: 40px;
+  color: rgb(55, 53, 47);
+  border: none;
+  outline: none;
+  max-width: 100%;
+  width: 100%;
+  white-space: pre-wrap;
+  word-break: break-word;
+  caret-color: rgb(55, 53, 47);
+  padding: 3px 2px;
+  min-height: 1em;
+  cursor: text;
+
+  &::placeholder {
+    color: rgba(55, 53, 47, 0.15);
+  }
 `;
 
 interface Props {}
 
 function Title({}: Props): JSX.Element {
-  const page = useRecoilValue(pageState('1'));
+  const [selectedPage, setSelectedPage] = useRecoilState(selectedPageState);
+  const setPages = useSetRecoilState(pagesState);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSelectedPage({ ...selectedPage, title: event.currentTarget.value });
+  };
 
   return (
     <div css={wrapperCss()}>
       <div css={titlePaddingTopCss()} />
-      <div css={titleCss()} contentEditable suppressContentEditableWarning>
-        {page?.title}
-      </div>
+      <input
+        css={titleCss()}
+        onChange={handleChange}
+        type="text"
+        placeholder="Untitled"
+      />
     </div>
   );
 }
