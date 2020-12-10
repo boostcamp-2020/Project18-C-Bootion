29aa2a87f9bacb380d579215cb8466f49b92fa1f
domino8788 <domino8788@naver.com>
2020-12-10 12:11:35 +0900

    [#90] feat: useFamily 구현사항을 index에 반영

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useCommand } from './useCommand';
 export { default as useManager } from './useManager';
+export { default as useFamily } from './useFamily';
 export { useApi } from './useApi';
