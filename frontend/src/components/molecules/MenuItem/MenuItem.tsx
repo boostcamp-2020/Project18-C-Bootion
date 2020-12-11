d64f33c317789bfbd8d886c7e387f101e68d7dbe
skid901 <tlrudejr1993@naver.com>
2020-12-11 09:20:52 +0900

    [#88] refactor: MenuItem 컴포넌트 내 페이지 삭제 버튼 추가

--- frontend/src/components/molecules/MenuItem/MenuItem.tsx
+++ frontend/src/components/molecules/MenuItem/MenuItem.tsx
@@ -1,21 +1,19 @@
 /** @jsx jsx */
 /** @jsxRuntime classic */
 import { css, jsx } from '@emotion/react';
-import { Page } from '@/schemes';
 import { useRecoilState } from 'recoil';
-import { selectedPageState } from '@/stores';
 import { useState } from 'react';
+
+import { Page } from '@/schemes';
+import { selectedPageState } from '@/stores';
+import { HeaderButton } from '@atoms/index';
+import { ReactComponent as Trash } from '@assets/trash.svg';
 
 const itemCss = (isHovered: boolean) => css`
   align-items: center;
   min-height: 27px;
   padding: 2px 14px;
-  width: 100%;
-  font-size: 14px;
-  line-height: 2;
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
+  padding-right: ${isHovered ? 8 : 14}px;
   cursor: pointer;
   background: ${isHovered ? 'rgba(55, 53, 47, 0.08)' : 'inherit'};
 `;
@@ -23,18 +21,23 @@ const selectedItemCss = (isHovered: boolean) => css`
   align-items: center;
   min-height: 27px;
   padding: 2px 14px;
-  width: 100%;
+  padding-right: ${isHovered ? 8 : 14}px;
+  background: ${isHovered
+    ? 'rgba(55, 53, 47, 0.16)'
+    : 'rgba(55, 53, 47, 0.08)'};
+  cursor: pointer;
+`;
+const titleCss = () => css`
+  color: rgb(55, 53, 47);
   font-size: 14px;
   font-weight: 600;
-  line-height: 2;
+  line-height: 2.1;
   overflow: hidden;
   white-space: nowrap;
   text-overflow: ellipsis;
-  background: ${isHovered
-    ? 'rgba(55, 53, 47, 0.18)'
-    : 'rgba(55, 53, 47, 0.08)'};
-  color: rgb(55, 53, 47);
-  cursor: pointer;
+`;
+const trashCss = () => css`
+  float: right;
 `;
 
 interface Props {
@@ -44,6 +47,8 @@ interface Props {
 function MenuItem({ page }: Props): JSX.Element {
   const [selectedPage, setSelectedPage] = useRecoilState(selectedPageState);
   const [hoverToggle, setHoverToggle] = useState(false);
+
+  const handleDeletePage = () => {};
 
   return (
     <div
@@ -58,7 +63,14 @@ function MenuItem({ page }: Props): JSX.Element {
       onMouseEnter={() => setHoverToggle(true)}
       onMouseLeave={() => setHoverToggle(false)}
     >
-      {page.title || 'Untitled'}
+      {hoverToggle && (
+        <div css={trashCss()}>
+          <HeaderButton handleClick={handleDeletePage}>
+            <Trash />
+          </HeaderButton>
+        </div>
+      )}
+      <div css={titleCss()}>{page.title || 'Untitled'}</div>
     </div>
   );
 }
