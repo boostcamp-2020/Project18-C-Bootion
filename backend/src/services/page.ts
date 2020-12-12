0faff29ad255575bd26baba247521ef1e7d28a04
skid901 <tlrudejr1993@naver.com>
2020-12-12 22:06:00 +0900

    [#105] feat: page 서비스 테스트 코드 작성

--- backend/src/services/page.ts
+++ backend/src/services/page.ts
@@ -1,46 +1,22 @@
-import { BlockModel, PageDoc, PageModel } from '@/models';
+import { PageDoc, PageDTO } from '@/models';
 import { ErrorMessage } from '@/aops';
 
-export const create = async (): Promise<PageDoc> => {
-  const page = new PageModel();
-  await page.save();
-  return page;
+export const create = async (pageDTO?: PageDTO): Promise<PageDoc> => {
+  return null;
 };
 
-export const getAll = async (): Promise<PageDoc[]> => {
-  return await PageModel.find({}, 'id title')
-    .sort([['createdAt', -1]])
-    .exec();
+export const readOne = async (pageDTO: PageDTO): Promise<PageDoc> => {
+  return null;
 };
 
-export const getOne = async (params: { id: string }): Promise<PageDoc> => {
-  const page = await PageModel.findById(params.id).populate('blockList').exec();
-
-  if (!page) {
-    throw new Error(ErrorMessage.NOT_FOUND);
-  }
-  await page.populateBlock();
-  return page;
+export const readAll = async (): Promise<PageDoc[]> => {
+  return null;
 };
 
-export const update = async (params: {
-  id: string;
-  title: string;
-}): Promise<PageDoc> => {
-  const page = await PageModel.findByIdAndUpdate(
-    params.id,
-    { title: params.title ?? '' },
-    { new: true },
-  ).exec();
-
-  if (!page) {
-    throw new Error(ErrorMessage.NOT_FOUND);
-  }
-  return page;
+export const update = async (pageDTO: PageDTO): Promise<PageDoc> => {
+  return null;
 };
 
-export const remove = async (params: { id: string }): Promise<void> => {
-  const page = await PageModel.findById(params.id).exec();
-  page && (await BlockModel.deleteMany({ pageId: { $eq: page.id } }));
-  await PageModel.findByIdAndDelete(page.id).exec();
+export const deleteOne = async (pageDTO: PageDTO): Promise<PageDTO> => {
+  return null;
 };
---
+++ backend/test/services/page.spec.ts
@@ -0,0 +1,99 @@
+import { config } from 'dotenv';
+import * as mongoose from 'mongoose';
+
+import { connect, PageDTO } from '@/models';
+import { pageService } from '@/services';
+import { ErrorMessage } from '@/aops';
+
+config();
+
+describe('@services/page', () => {
+  const dbName = 'bootionTest';
+  let conn: mongoose.Connection;
+
+  beforeAll(async () => {
+    await connect(dbName);
+    conn = mongoose.connections[0];
+  });
+
+  beforeEach(async () => {
+    //
+  });
+
+  afterEach(async () => {
+    await conn.dropDatabase();
+  });
+
+  afterAll(async () => {
+    try {
+      await conn.dropDatabase();
+      await conn.close();
+      console.log('MongoDB is disconnected');
+    } catch (e) {
+      console.error('MongoDB disconnection failed by', e);
+    }
+  });
+
+  it('create: Success', async () => {
+    const pageDTO: PageDTO = { title: 'test page title' };
+    const received = await pageService.create(pageDTO);
+    expect(received?.toJSON().title).toEqual(pageDTO.title);
+  });
+
+  it('create: Duplicated error', async () => {
+    await expect(async () => {
+      const page = await pageService.create();
+      await pageService.create(page.toJSON());
+    }).rejects.toThrow();
+  });
+
+  it('readOne: Success', async () => {
+    const expected = await pageService.create();
+    const received = await pageService.readOne(expected.toJSON());
+    expect(received).toEqual(expected);
+  });
+
+  it('readOne: Not found', async () => {
+    await expect(async () => {
+      const pageDTO: PageDTO = { id: 'invalid id' };
+      await pageService.readOne(pageDTO);
+    }).rejects.toThrow(ErrorMessage.NOT_FOUND);
+  });
+
+  it('readAll: Success', async () => {
+    const page01 = await pageService.create({ title: 'test page 01' });
+    const page02 = await pageService.create({ title: 'test page 02' });
+    const expected = [page01, page02].reverse();
+    const received = await pageService.readAll();
+    expect(received).toEqual(expected);
+  });
+
+  it('update: Success', async () => {
+    const page = await pageService.create();
+    const expected: PageDTO = { ...page.toJSON(), title: 'updated title' };
+    const received = await pageService.update(expected);
+    expect(received?.toJSON()).toEqual(expected);
+  });
+
+  it('update: Not found', async () => {
+    await expect(async () => {
+      const pageDTO: PageDTO = { id: 'invalid id' };
+      await pageService.update(pageDTO);
+    }).rejects.toThrow(ErrorMessage.NOT_FOUND);
+  });
+
+  it('delete: Success', async () => {
+    const pageDTO = (await pageService.create())?.toJSON();
+    const received = await pageService.deleteOne(pageDTO);
+    await expect(async () => {
+      await pageService.readOne(received);
+    }).rejects.toThrow(ErrorMessage.NOT_FOUND);
+  });
+
+  it('delete: Not found', async () => {
+    await expect(async () => {
+      const pageDTO: PageDTO = { id: 'invalid id' };
+      await pageService.deleteOne(pageDTO);
+    }).rejects.toThrow(ErrorMessage.NOT_FOUND);
+  });
+});
