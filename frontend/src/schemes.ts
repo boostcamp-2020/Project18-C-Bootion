a27e7a4178e3afd75fa1e4f6274ae2072749deb4
domino8788 <domino8788@naver.com>
2020-12-12 22:04:08 +0900

    [#107] refactor: 데이터 모델 추가 변경사항 반영

--- frontend/src/schemes.ts
+++ frontend/src/schemes.ts
@@ -5,9 +5,9 @@ export enum BlockType {
   HEADING1 = 'heading1',
   HEADING2 = 'heading2',
   HEADING3 = 'heading3',
-  BULLETEDLIST = 'bulletedlist',
-  NUMBEREDLIST = 'numberedlist',
-  TOGGLELIST = 'togglelist',
+  BULLETED_LIST = 'bulletedlist',
+  NUMBERED_LIST = 'numberedlist',
+  TOGGLE_LIST = 'togglelist',
   QUOTE = 'quote',
   PAGE = 'page',
 }
@@ -18,8 +18,8 @@ export interface Block {
   id: IdType;
   type: BlockType;
   value: string;
-  childrenIdList: IdType[];
-  parentBlockId: IdType | null;
+  childIdList: IdType[];
+  parentId: IdType | null;
   pageId: IdType;
 }
 
@@ -32,8 +32,11 @@ export interface BlockFamily {
   parentIndex: number | null;
   grandParent: Block | null;
   page: Page;
+  childrenIdList: IdType[];
   children: Block[];
+  siblingsIdList: IdType[];
   siblings: Block[];
+  parentsIdList: IdType[];
   parents: Block[];
 }
 
@@ -41,16 +44,12 @@ export interface FamilyFunc {
   getNextBlock: () => Block | null;
   getPrevBlock: () => Block | null;
   setBlockMap: (blockMap: BlockMap) => void;
-  setBlock: (block: Block) => void;
-  setParent: (block: Block) => void;
-  setGrandParent: (block: Block) => void;
-  setPage: (page: Page) => void;
 }
 
 export interface Page {
   id: IdType;
   title: string;
-  blockIdList: IdType[];
+  rootId: IdType;
 }
 
 export interface User {
