76ad23f42dce5191555be7f0af77b5e73ecbd09c
skid901 <tlrudejr1993@naver.com>
2020-12-13 00:49:11 +0900

    [#105] refactor(test/services/page.spec.ts): 테스트 코드 리팩토링

--- backend/test/services/page.spec.ts
+++ backend/test/services/page.spec.ts
@@ -37,27 +37,31 @@ describe('@services/page', () => {
   it('create: Success', async () => {
     const pageDTO: PageDTO = { title: 'test page title' };
     const received = await pageService.create(pageDTO);
+
     expect(received?.toJSON().title).toEqual(pageDTO.title);
   });
 
   it('create: Duplicated error', async () => {
-    await expect(async () => {
-      const page = await pageService.create();
-      await pageService.create(page.toJSON());
-    }).rejects.toThrow();
+    const received = await pageService.create();
+
+    await expect(async () =>
+      pageService.create(received.toJSON()),
+    ).rejects.toThrow();
   });
 
   it('readOne: Success', async () => {
     const expected = await pageService.create();
     const received = await pageService.readOne(expected.id);
+
     expect(received?.toJSON()).toEqual(expected?.toJSON());
   });
 
   it('readOne: Not found', async () => {
-    await expect(async () => {
-      const pageDTO: PageDTO = { id: 'invalid id' };
-      await pageService.readOne(pageDTO.id);
-    }).rejects.toThrow();
+    const received: PageDTO = { id: 'invalid id' };
+
+    await expect(async () =>
+      pageService.readOne(received.id),
+    ).rejects.toThrow();
   });
 
   it('readAll: Success', async () => {
@@ -69,6 +73,7 @@ describe('@services/page', () => {
     const received = (await pageService.readAll()).map((pageDoc) =>
       pageDoc.toJSON(),
     );
+
     expect(received).toEqual(expected);
   });
 
@@ -76,36 +81,41 @@ describe('@services/page', () => {
     const page = await pageService.create();
     const expected: PageDTO = { ...page.toJSON(), title: 'updated title' };
     const received = await pageService.update(expected.id, expected);
+
     expect(received?.toJSON().title).toEqual(expected.title);
   });
 
   it('update: Not found', async () => {
-    await expect(async () => {
-      const pageDTO: PageDTO = { id: 'invalid id' };
-      await pageService.update(pageDTO.id, pageDTO);
-    }).rejects.toThrow();
+    const received: PageDTO = { id: 'invalid id' };
+
+    await expect(
+      async () => await pageService.update(received.id, received),
+    ).rejects.toThrow();
   });
 
   it('update: Bad request', async () => {
-    await expect(async () => {
-      const pageDTO: PageDTO = (await pageService.create()).toJSON();
-      delete pageDTO.title;
-      await pageService.update(pageDTO.id, pageDTO);
-    }).rejects.toThrow(ErrorMessage.BAD_REQUEST);
+    const received: PageDTO = (await pageService.create()).toJSON();
+    delete received.title;
+
+    await expect(async () =>
+      pageService.update(received.id, received),
+    ).rejects.toThrow(ErrorMessage.BAD_REQUEST);
   });
 
   it('delete: Success', async () => {
     const pageDTO: PageDTO = (await pageService.create()).toJSON();
     const received = await pageService.deleteOne(pageDTO.id);
-    await expect(async () => {
-      await pageService.readOne(received.id);
-    }).rejects.toThrow();
+
+    await expect(
+      async () => await pageService.readOne(received.id),
+    ).rejects.toThrow();
   });
 
   it('delete: Not found', async () => {
-    await expect(async () => {
-      const pageDTO: PageDTO = { id: 'invalid id' };
-      await pageService.deleteOne(pageDTO.id);
-    }).rejects.toThrow();
+    const received: PageDTO = { id: 'invalid id' };
+
+    await expect(
+      async () => await pageService.deleteOne(received.id),
+    ).rejects.toThrow();
   });
 });
