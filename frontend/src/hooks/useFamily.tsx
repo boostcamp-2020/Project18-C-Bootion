dc89becf8abd891242192ae4221be0c1b25d8eb5
domino8788 <domino8788@naver.com>
2020-12-12 22:15:26 +0900

    [#107] refactor: useFamily 리팩토링
    - 데이터 모델 변경사항 반영
    - 들여쓰기를 최소로 하게 수정

--- frontend/src/hooks/useFamily.tsx
+++ frontend/src/hooks/useFamily.tsx
@@ -1,45 +1,38 @@
 /* eslint-disable @typescript-eslint/indent */
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { pageState, blockMapState } from '@/stores';
-import {
-  Block,
-  BlockType,
-  BlockFamily,
-  FamilyFunc,
-  IdType,
-  Page,
-} from '@/schemes';
+import { Block, BlockType, BlockFamily, FamilyFunc, IdType } from '@/schemes';
 import { useBlock } from '@/hooks';
 
 const useFamily = (blockId: string): [BlockFamily, FamilyFunc] => {
   const [blockMap, setBlockMap] = useRecoilState(blockMapState);
   const [block, setBlock] = useBlock(blockId);
-  const [page, setPage] = useRecoilState(pageState);
-  const [parent, setParent] = useBlock(block?.parentBlockId);
-  const [grandParent, setGrandParent] = useBlock(parent?.parentBlockId);
-
+  const [parent, setParent] = useBlock(block?.parentId);
+  const [grandParent, setGrandParent] = useBlock(parent?.parentId);
+  const [greatGrandParent, setGreatGrandParent] = useBlock(
+    grandParent?.parentId,
+  );
+  const page = useRecoilValue(pageState);
   const getChildren = (childrenIdList: IdType[] | null) =>
     childrenIdList?.map((childId: string) => blockMap[childId]);
-
-  const children = getChildren(block?.childrenIdList);
-  const siblings = getChildren(parent?.childrenIdList || page?.blockIdList);
-  const parents = getChildren(
-    grandParent?.childrenIdList || (parent && page.blockIdList),
-  );
-  const blockIndex = (parent?.childrenIdList || page.blockIdList).findIndex(
+  const childrenIdList = block?.childIdList;
+  const children = getChildren(childrenIdList);
+  const siblingsIdList = parent?.childIdList;
+  const siblings = getChildren(siblingsIdList);
+  const parentsIdList = grandParent?.childIdList;
+  const parents = getChildren(parentsIdList);
+  const blockIndex = parent?.childIdList?.findIndex(
     (_blockId: string) => _blockId === block?.id,
   );
-  const parentIndex = (
-    grandParent?.childrenIdList || page.blockIdList
-  ).findIndex((_blockId: string) => _blockId === parent?.id);
+  const parentIndex = grandParent?.childIdList?.findIndex(
+    (_blockId: string) => _blockId === parent?.id,
+  );
 
   const findLastDescendant = (targetBlock: Block) => {
     let currentBlock = targetBlock;
-    while (currentBlock.childrenIdList.length) {
+    while (currentBlock.childIdList.length) {
       currentBlock =
-        blockMap[
-          currentBlock.childrenIdList[currentBlock.childrenIdList.length - 1]
-        ];
+        blockMap[currentBlock.childIdList[currentBlock.childIdList.length - 1]];
     }
     return currentBlock;
   };
@@ -49,87 +42,76 @@ const useFamily = (blockId: string): [BlockFamily, FamilyFunc] => {
       /* 자식이 있을 때 첫번째 자식이 다음 block 이다.  */
       return children[0];
     }
-    if (blockIndex === siblings.length - 1) {
-      /* block이 마지막 자식일 때. 다음 부모가 다음 Block 이다. */
-      const targetParentBlock = parents[parentIndex + 1];
-      if (targetParentBlock) {
-        switch (targetParentBlock.type) {
-          case BlockType.COLUMN:
-            /** 다음 부모의 Type이 Column 일때 첫번째 자식이 다음 Block 이다. */
-            return blockMap[targetParentBlock.childrenIdList[0]];
-          case BlockType.GRID:
-            /** 다음 부모의 Type이 Grid 일때 첫번째 자식(Column)의 자식이 다음 Block 이다. */
-            return blockMap[
-              blockMap[targetParentBlock.childrenIdList[0]].childrenIdList[0]
-            ];
-          default:
-            /** 다음 부모의 타입이 COLUMN이나 GRID가 아니면 다음 Block 이다. */
-            return targetParentBlock;
-        }
-      } else {
-        /** 현재 블록이 마지막 블록이다. */
-        return null;
-      }
-    } else {
+    if (blockIndex !== siblings.length - 1) {
       /** 자식이 없고 마지막 자식이 아니므로 다음 형제가 다음 Block 이다.  */
       return siblings[blockIndex + 1];
     }
+    /* block이 마지막 자식일 때. 다음 부모가 다음 Block 이다. */
+    const targetParentBlock = parents[parentIndex + 1];
+    if (targetParentBlock) {
+      switch (targetParentBlock.type) {
+        case BlockType.COLUMN:
+          /** 다음 부모의 Type이 Column 일때 첫번째 자식이 다음 Block 이다. */
+          return blockMap[targetParentBlock.childIdList[0]];
+        case BlockType.GRID:
+          /** 다음 부모의 Type이 Grid 일때 첫번째 자식(Column)의 자식이 다음 Block 이다. */
+          return blockMap[
+            blockMap[targetParentBlock.childIdList[0]].childIdList[0]
+          ];
+        default:
+          /** 다음 부모의 타입이 COLUMN이나 GRID가 아니면 다음 Block 이다. */
+          return targetParentBlock;
+      }
+    }
+    /** 현재 블록이 마지막 블록이다. */
+    return null;
   };
 
   const getPrevBlock = () => {
     if (blockIndex) {
       /** blockIndex가 0이 아니면 이전 형제에서 prev block을 찾을 수 있다. */
       const prevSibling = siblings[blockIndex - 1];
-      if (prevSibling?.childrenIdList.length) {
+      if (prevSibling?.childIdList.length) {
         /* 이전 형제에게 자식이 있다면 마지막 자식이 prev block 이다.  */
         return blockMap[
-          prevSibling.childrenIdList[prevSibling.childrenIdList.length - 1]
+          prevSibling.childIdList[prevSibling.childIdList.length - 1]
         ];
       }
       /* 이전 형제에게 자식이 없다면 형제가 prev block 이다.  */
       return prevSibling;
     }
     /* block이 첫번째 자식일 때. 부모가 이전 Block 이다. */
-    if (parent) {
-      switch (parent.type) {
-        case BlockType.COLUMN: {
-          /** 부모의 Type이 Column 이면 부모의 이전 형제의 마지막 자식이 이전 Block 이다. */
-          if (parentIndex) {
-            /** 부모의 index가 0이 아니면 이전 부모의 마지막 */
-            const prevParentChildren = getChildren(
-              parents[parentIndex - 1].childrenIdList,
-            );
-            return prevParentChildren[prevParentChildren.length - 1];
-          }
-          /** 부모의 index가 0이면 조부모에 이전 Block이 있다. */
-          if (grandParent) {
-            /** grandParent가 있을 경우 조부모의 마지막 자손이 이전 Block 이다. */
-            if (grandParent.type === BlockType.GRID) {
-              /** 조부모가 GRID 타입인 경우 조부모의 부모는 Page 이다. */
-              const grandParentIndex = page.blockIdList.findIndex(
-                (_blockId: string) => _blockId === grandParent.id,
-              );
-              if (grandParentIndex) {
-                /** grandParentIndex 가 0이 아니면 prevGrandParent의 마지막 후손이 prev 블록이다. */
-                const prevGrandParent =
-                  blockMap[page.blockIdList[grandParentIndex - 1]];
-                return findLastDescendant(prevGrandParent);
-              }
-              /** grandParentIndex가 0이면 page의 blockList 중 맨 앞이라는 의미이므로 prev 블록이 없다. */
-              return null;
-            }
-            return grandParent;
-          }
-          /** grandParent가 없을 경우 이전 블록이 없다는 뜻이다. */
-          return null;
-        }
-        default:
-          return parent;
-      }
-    } else {
-      /** 현재 블록이 첫 블록이다. */
+    if (parent.type !== BlockType.COLUMN) {
+      return parent.parentId ? parent : null;
+    }
+    /** 부모의 Type이 Column 이면 부모의 이전 형제의 마지막 자식이 이전 Block 이다. */
+    if (parentIndex) {
+      /** 부모의 index가 0이 아니면 이전 부모의 마지막 */
+      const prevParentChildren = getChildren(
+        parents[parentIndex - 1].childIdList,
+      );
+      return prevParentChildren[prevParentChildren.length - 1];
+    }
+    /** 부모의 index가 0이면 조부모에 이전 Block이 있다. */
+    if (!grandParent) {
+      /** grandParent가 없을 경우 이전 블록이 없다는 뜻이다. */
       return null;
     }
+    if (grandParent.type !== BlockType.GRID) {
+      return grandParent;
+    }
+    /** 조부모가 GRID 타입인 경우 조부모의 이전 형제에 이전 블록이 있다. */
+    const grandParentIndex = greatGrandParent.childIdList.findIndex(
+      (_blockId: string) => _blockId === grandParent.id,
+    );
+    if (!grandParentIndex) {
+      /** grandParentIndex가 0이면 page의 blockList 중 맨 앞이라는 의미이므로 prev 블록이 없다. */
+      return null;
+    }
+    /** grandParentIndex 가 0이 아니면 prevGrandParent의 마지막 후손이 prev 블록이다. */
+    const prevGrandParent =
+      blockMap[greatGrandParent.childIdList[grandParentIndex - 1]];
+    return findLastDescendant(prevGrandParent);
   };
 
   return [
@@ -141,18 +123,17 @@ const useFamily = (blockId: string): [BlockFamily, FamilyFunc] => {
       parentIndex,
       grandParent,
       page,
+      childrenIdList,
       children,
+      siblingsIdList,
       siblings,
+      parentsIdList,
       parents,
     },
     {
       getNextBlock,
       getPrevBlock,
       setBlockMap,
-      setBlock,
-      setParent,
-      setGrandParent,
-      setPage,
     },
   ];
 };
