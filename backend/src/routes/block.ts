f2298df5812703d57aea82667383b3cacdb6d20e
skid901 <tlrudejr1993@naver.com>
2020-12-14 12:03:11 +0900

    [#105] refactor(@routes/blocks): 서비스 변경에 따라 리팩토링

--- backend/src/routes/block.ts
+++ backend/src/routes/block.ts
@@ -2,8 +2,32 @@ import { Router } from 'express';
 
 import { blockController } from '@/controllers';
 import { errorHandler } from '@/aops';
+import { validateObjectId } from '@/middlewares';
 
 export const blockRouter = Router();
 
-blockRouter.post('', errorHandler(blockController.create));
-blockRouter.patch('', errorHandler(blockController.updateOrRemove));
+blockRouter.post(
+  '/parent-id/:parentId',
+  validateObjectId('parentId'),
+  errorHandler(blockController.create),
+);
+blockRouter.get(
+  '/page-id/:pageId',
+  validateObjectId('pageId'),
+  errorHandler(blockController.readAll),
+);
+blockRouter.patch(
+  '/id/:blockId',
+  validateObjectId('blockId'),
+  errorHandler(blockController.update),
+);
+blockRouter.patch(
+  '/id/:blockId/to/:toId',
+  validateObjectId('blockId', 'toId'),
+  errorHandler(blockController.move),
+);
+blockRouter.delete(
+  '/id/:blockId',
+  validateObjectId('blockId'),
+  errorHandler(blockController.deleteCascade),
+);
