6f27e09c8ed3decb110a44fa49d7aca6e03a8bf7
skid901 <tlrudejr1993@naver.com>
2020-12-14 12:01:58 +0900

    [#105] refactor(@controllers/page): API 반환 값을 객체로 묶어서 전달하도록 수정

--- backend/src/controllers/page.ts
+++ backend/src/controllers/page.ts
@@ -12,24 +12,24 @@ export const create = transactionHandler(
 );
 
 export const readOne = async (req: Request, res: Response): Promise<void> => {
-  const page = await pageService.readOne(req.params.id);
-  res.status(StatusCode.OK).json(page);
+  const page = await pageService.readOne(req.params.pageId);
+  res.status(StatusCode.OK).json({ page });
 };
 
 export const readAll = async (req: Request, res: Response): Promise<void> => {
   const pages = await pageService.readAll();
-  res.status(StatusCode.OK).json(pages);
+  res.status(StatusCode.OK).json({ pages });
 };
 
 export const update = async (req: Request, res: Response): Promise<void> => {
-  const page = await pageService.update(req.params.id, req.body.page);
-  res.status(StatusCode.OK).json(page);
+  const page = await pageService.update(req.params.pageId, req.body.page);
+  res.status(StatusCode.OK).json({ page });
 };
 
 export const deleteOne = transactionHandler(
   async (req: Request, res: Response): Promise<void> => {
-    await pageService.deleteOne(req.params.id);
+    await pageService.deleteOne(req.params.pageId);
     const pages = await pageService.readAll();
-    res.status(StatusCode.OK).json(pages);
+    res.status(StatusCode.OK).json({ pages });
   },
 );
