f047ddd4e6ad742ead990935b119a5d5f3cc41be
Yi SoJeong <soj980312@gmail.com>
2020-12-20 01:12:06 +0900

    [#146] feat: 시간 경과 계산 함수 생성

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,11 +1,7 @@
 export { fetchDummyData } from './fetchDummyData';
-export {
-  validateType,
-  fontSize,
-  placeHolder,
-  listComponent,
-} from './blockContent';
+export * from './blockContent';
 export * from './debounce';
 export * from './fetchApi';
 export * from './blockApis';
 export * from './pageApis';
+export * from './time';
