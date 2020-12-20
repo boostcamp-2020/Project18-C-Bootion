d3b992ce9827cb5c11e1fb9c87a8f5a56f2703a9
domino8788 <domino8788@naver.com>
2020-12-20 10:39:12 +0900

    [#152] feat: 세션 적용 및 소켓 연결

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -1,13 +1,21 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import logger from 'morgan';
 import createHttpError, { HttpError } from 'http-errors';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+import flash from 'connect-flash';
+import { config } from 'dotenv';
 
+import webSocket from '@/socket';
+import { StatusCode, ErrorMessage } from '@/aops';
 import { connect } from '@/models';
 import { apiRouter } from '@/routes';
-import { StatusCode, ErrorMessage } from '@/aops';
+
+config();
 
 export class App {
   private app: Application;
+  private sessionMiddleware: any;
 
   static bootstrap(port: number) {
     const app = new App();
@@ -18,6 +26,15 @@ export class App {
     connect();
 
     this.app = express();
+    this.sessionMiddleware = session({
+      resave: false,
+      saveUninitialized: false,
+      secret: process.env.COOKIE_SECRET,
+      cookie: {
+        httpOnly: true,
+        secure: false,
+      },
+    });
 
     this.initMiddlewares();
     this.initRouters();
@@ -28,6 +45,9 @@ export class App {
     this.app.use(logger('dev'));
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(cookieParser(process.env.COOKIE_SECRET));
+    this.app.use(this.sessionMiddleware);
+    this.app.use(flash());
   }
 
   private initRouters() {
@@ -48,9 +68,10 @@ export class App {
   }
 
   private listen(port: number): void {
-    this.app
+    const server = this.app
       .listen(port, () => console.log(`Express server listening at ${port}`))
       .on('error', (err) => console.error(err));
+    webSocket(server, this.app, this.sessionMiddleware);
   }
 }
 
