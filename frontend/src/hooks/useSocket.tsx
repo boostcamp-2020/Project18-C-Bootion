8e87136b78c95041a28ad9e5af4c21f950ece4a0
domino8788 <domino8788@naver.com>
2020-12-20 20:23:07 +0900

    [#153] feat: 페이지 내 유저수와 최종 업데이트 시간 상태 갱신 로직 구현

--- frontend/src/hooks/useSocket.tsx
+++ frontend/src/hooks/useSocket.tsx
@@ -3,23 +3,23 @@ import { blockMapState, pagesState } from '@/stores';
 import { pageIO, pageListIO } from '@/socket';
 import { BlockMap, Page } from '@/schemes';
 import { useEffect } from 'react';
+import { pageUserCountState, lastUpdateState } from '@/stores/page';
+
+const doSomeThing = () => {};
 
 const useSocket = () => {
   const setBlockMap = useSetRecoilState(blockMapState);
   const setPages = useSetRecoilState(pagesState);
+  const setPageUserCount = useSetRecoilState(pageUserCountState);
+  const setLastUpdate = useSetRecoilState(lastUpdateState);
 
   useEffect(() => {
     pageListIO.on('PageListUpdate', (pages: Page[]) => {
-      console.log(pages);
       setPages(pages);
     });
 
     pageListIO.on('allUserCount', (count: number) => {
       console.log(`allUserCount: ${count}`);
-    });
-
-    pageIO.on('connect', () => {
-      console.log('pageIO connect');
     });
 
     pageIO.on('connect_error', () => {
@@ -28,28 +28,35 @@ const useSocket = () => {
       }, 1000);
     });
 
-    pageIO.on('disconnect', (reason: any) => {
-      console.log(reason);
-    });
-
     pageIO.on('PageUpdate', (updatedBlockMap: BlockMap) => {
       const { focusOffset: beforeOffset, focusNode } = window.getSelection();
-      const length = (focusNode as any)?.length;
-      if (length) {
+      const beforeLength = (focusNode as any)?.length;
+      if (beforeLength) {
         focusNode.parentElement.blur();
         setTimeout(() => {
-          const sel = window.getSelection();
-          sel.collapse(
-            sel.focusNode,
-            beforeOffset < length ? beforeOffset : length,
-          );
+          try {
+            const sel = window.getSelection();
+            const afterLength = (focusNode as any)?.length;
+            if (
+              focusNode !== sel.focusNode ||
+              beforeOffset !== sel.focusOffset
+            ) {
+              sel.collapse(
+                sel.focusNode,
+                beforeOffset < afterLength ? beforeOffset : afterLength,
+              );
+            }
+          } catch (e: any) {
+            doSomeThing();
+          }
         });
       }
+      setLastUpdate(new Date());
       setBlockMap(updatedBlockMap);
     });
 
     pageIO.on('pageUserCount', (count: number) => {
-      console.log(`pageUserCount: ${count}`);
+      setPageUserCount(count);
     });
   }, []);
 };
