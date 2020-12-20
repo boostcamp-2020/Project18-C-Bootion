28cb69b781c55c4eb0bc5eb6da2017ad19ad7f18
skid901 <tlrudejr1993@naver.com>
2020-12-21 02:33:09 +0900

    fix: socket.io 오류 해결을 위해 엔진엑스 설정 추가

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,12 +1,12 @@
 import { io } from 'socket.io-client';
 
-export const pageIO = io(':4000/page', {
+export const pageIO = io('/page', {
   path: '/socket.io',
   transports: ['websocket'],
   reconnection: true,
 });
 
-export const pageListIO = io(':4000/pageList', {
+export const pageListIO = io('/pageList', {
   path: '/socket.io',
   transports: ['websocket'],
 });
