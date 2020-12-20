2203d905b39c1727df56c1f0a7936d3d77c910ed
domino8788 <domino8788@naver.com>
2020-12-20 17:10:43 +0900

    [#153] feat: useSocket 구현사항 반영

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -2,5 +2,6 @@ export { default as useCommand } from './useCommand';
 export { default as useManager } from './useManager';
 export { default as useFamily } from './useFamily';
 export { default as useBlock } from './useBlock';
+export { default as useSocket } from './useSocket';
 
 export { useApi } from './useApi';
